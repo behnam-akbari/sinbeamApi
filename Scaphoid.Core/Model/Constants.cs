95e90c680d5ab7720e6dd64e22f55dbe741d921d
akbari <makbari.it@gmail.com>
2024-03-01 10:38:06 +0330

    in prigress

--- Scaphoid.Core/Model/Constants.cs
+++ Scaphoid.Core/Model/Constants.cs
@@ -2,87 +2,85 @@
 {
     public static class Constants
     {
-        public static List<double> WebThicknessCollection = new() { 1.5, 2, 2.5, 3 };
+        //public static List<int> FlangeThicknessCollection = new() { 8, 10, 12, 15, 20, 25, 30, 40 };
 
-        public static List<int> FlangeThicknessCollection = new() { 8, 10, 12, 15, 20, 25, 30, 40 };
+        //public static List<double> FlangeWidthCollection = new() { 160, 180, 200, 220, 250, 300, 350, 400 };
 
-        public static List<double> FlangeWidthCollection = new() { 160, 180, 200, 220, 250, 300, 350, 400 };
+        //public static List<double> FlangeWidthCollection2 = new()
+        //{
+        //    160,
+        //    180,
+        //    200,
+        //    160,
+        //    180,
+        //    200,
+        //    220,
+        //    200,
+        //    220,
+        //    200,
+        //    250,
+        //    220,
+        //    250,
+        //    220,
+        //    300,
+        //    250,
+        //    300,
+        //    250,
+        //    350,
+        //    300,
+        //    350,
+        //    300,
+        //    400,
+        //    350,
+        //    300,
+        //    400,
+        //    350,
+        //    300,
+        //    300,
+        //    400,
+        //    350,
+        //    350,
+        //    400,
+        //    400
+        //};
 
-        public static List<double> FlangeWidthCollection2 = new()
-        {
-            160,
-            180,
-            200,
-            160,
-            180,
-            200,
-            220,
-            200,
-            220,
-            200,
-            250,
-            220,
-            250,
-            220,
-            300,
-            250,
-            300,
-            250,
-            350,
-            300,
-            350,
-            300,
-            400,
-            350,
-            300,
-            400,
-            350,
-            300,
-            300,
-            400,
-            350,
-            350,
-            400,
-            400
-        };
-
-        public static List<int> FlangeThicknessCollection2 = new()
-        {
-            6,
-            6,
-            6,
-            8,
-            8,
-            8,
-            8,
-            10,
-            10,
-            12,
-            10,
-            12,
-            12,
-            15,
-            12,
-            15,
-            15,
-            20,
-            15,
-            20,
-            20,
-            25,
-            20,
-            25,
-            30,
-            25,
-            30,
-            35,
-            40,
-            30,
-            35,
-            40,
-            35,
-            40
-        };
+        //public static List<int> FlangeThicknessCollection2 = new()
+        //{
+        //    6,
+        //    6,
+        //    6,
+        //    8,
+        //    8,
+        //    8,
+        //    8,
+        //    10,
+        //    10,
+        //    12,
+        //    10,
+        //    12,
+        //    12,
+        //    15,
+        //    12,
+        //    15,
+        //    15,
+        //    20,
+        //    15,
+        //    20,
+        //    20,
+        //    25,
+        //    20,
+        //    25,
+        //    30,
+        //    25,
+        //    30,
+        //    35,
+        //    40,
+        //    30,
+        //    35,
+        //    40,
+        //    35,
+        //    40
+        //};
 
         public static readonly DesignParameters UkNA = new()
         {
@@ -159,7 +157,6 @@
             Width = width;
             Thickness = thickness;
             It = it;
-
         }
 
         public int Width { get; set; }
